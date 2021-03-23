15212b466c8872f693266c962f62d62efb3e0d47
ubuntu <ubuntu@pm.me>
2021-03-23 23:25:40 +0000

    mod 1h 24mar21

--- run_corr_1hr_24mar21.py
+++ run_corr_1hr_24mar21.py
@@ -13,7 +13,7 @@ d.put_dict('/cmd/corr/docopy','True')
 os.system('/usr/local/bin/dsacon corr start')
 time.sleep(20)
 os.system('/usr/local/bin/dsacon corr set')
-time.sleep(300)
+time.sleep(3600)
 
 d.put_dict('/cmd/corr/0', {'cmd': 'trigger', 'val': '0'})
 time.sleep(120)
