b2073fbdff40d6a1894dd078813ed98a8cf454cb
ubuntu <ubuntu@pm.me>
2021-04-14 02:34:18 +0000

    scheduler

---
+++ run_schedule.py
@@ -0,0 +1,77 @@
+import sys
+import os
+import time as pytime
+from time import sleep
+from datetime import datetime
+from datetime import timezone
+import numpy as np
+from dsautils import dsa_store
+
+def pause_until(time):
+    """
+    Pause your program until a specific end time.
+    'time' is either a valid datetime object or unix timestamp in seconds (i.e. seconds since Unix epoch)
+    """
+    end = time
+
+    # Convert datetime to unix timestamp
+    if isinstance(time, datetime):
+        end = time.timestamp()
+
+    # Type check
+    if not isinstance(end, (int, float)):
+        raise Exception('The time parameter is not a number or datetime object')
+
+    # Now we wait
+    while True:
+        now = datetime.now().astimezone(timezone.utc).timestamp()
+        diff = end - now
+        print('waiting: ',diff)
+
+        #
+        # Time is up!
+        #
+        if diff <= 0:
+            break
+        else:
+            # 'logarithmic' sleeping to minimize loop iterations
+            sleep(diff / 2)
+
+            
+def exec_action(a,d):
+    
+    if a['cmd'] == 'move':
+        d.put_dict('/cmd/ant/0', {'cmd': 'move', 'val': a['val']})
+        
+    if a['cmd'] == 'start':
+        d.put_dict('/cmd/corr/docopy','True')
+        os.system('/usr/local/bin/dsacon corr start')
+        pytime.sleep(60)
+        os.system('/usr/local/bin/dsacon corr set')
+        
+    if a['cmd'] == 'stop':
+        d.put_dict('/cmd/corr/0', {'cmd': 'trigger', 'val': '0'})
+        pytime.sleep(120)
+        os.system('/usr/local/bin/dsacon corr stop')
+        pytime.sleep(60)
+        d.put_dict('/cmd/corr/docopy','False')
+        
+    if a['cmd'] == 'test':
+        d.put_dict('/cmd/corr/100', {'cmd': 'test', 'val': '0'})
+        pytime.sleep(1)
+        os.system('echo tested')
+        
+# main part of code
+
+schedule = 'actions.npy'
+
+d = dsa_store.DsaStore()
+a = np.load(schedule,allow_pickle=True)
+for ln in a:
+
+    print(ln)
+    pause_until(ln['time'].datetime)
+    exec_action(ln,d)
+
+
+
