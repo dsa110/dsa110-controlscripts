a4e5425b52ae00f0d1ccf97c3bef430c7c9afc5b
dsa-storage user <user@dsa-storage.ovro.pvt>
2021-04-21 10:18:20 -0700

    update string to 21apr21

--- run_schedule.py
+++ run_schedule.py
@@ -75,7 +75,7 @@ schedule = '/home/ubuntu/proj/websrv/temp-clone/actions.npy'
 #schedule = 'actions.npy'
 
 d = dsa_store.DsaStore()
-d.put_dict('/cnf/datestring','19apr21')
+d.put_dict('/cnf/datestring','21apr21')
 
 a = np.load(schedule,allow_pickle=True)
 for ln in a:
