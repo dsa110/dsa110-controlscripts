ffa8ec22fa9b7b3936125fe1b7adce7418361ad1
ubuntu <ubuntu@pm.me>
2021-05-04 00:14:27 +0000

    04may21

--- run_schedule.py
+++ run_schedule.py
@@ -75,7 +75,7 @@ schedule = '/home/ubuntu/proj/websrv/temp-clone/actions.npy'
 #schedule = 'actions.npy'
 
 d = dsa_store.DsaStore()
-d.put_dict('/cnf/datestring','01may21')
+d.put_dict('/cnf/datestring','04may21')
 
 a = np.load(schedule,allow_pickle=True)
 for ln in a:
