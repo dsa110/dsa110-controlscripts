8605b62ff8d112bc6f4ab5f3d3ea8f387371f580
dsa-storage user <user@dsa-storage.ovro.pvt>
2021-06-14 22:01:46 -0700

    14jun21

--- run_schedule.py
+++ run_schedule.py
@@ -78,7 +78,7 @@ schedule = '/home/ubuntu/proj/websrv/temp-clone/actions.npy'
 #schedule = 'actions.npy'
 
 d = dsa_store.DsaStore()
-d.put_dict('/cnf/datestring','11jun21')
+d.put_dict('/cnf/datestring','14jun21')
 
 a = np.load(schedule,allow_pickle=True)
 for ln in a:
