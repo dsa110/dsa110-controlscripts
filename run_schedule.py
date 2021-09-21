c971e1ee0b8d89b101667c66ce175aef11dbd386
ubuntu <ubuntu@pm.me>
2021-09-21 06:23:00 +0000

    new actions

--- run_schedule.py
+++ run_schedule.py
@@ -79,7 +79,7 @@ schedule = '/home/ubuntu/proj/websrv/temp-clone/actions.npy'
 #schedule = 'actions.npy'
 
 d = dsa_store.DsaStore()
-d.put_dict('/cnf/datestring','FAST_FRINGE')
+d.put_dict('/cnf/datestring','FAST_FRINGEY')
 
 a = np.load(schedule,allow_pickle=True)
 for ln in a:
