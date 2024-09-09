801840e867ae14723361b68267e894c40bd903eb
VR-DSA <vikram@caltech.edu>
2024-09-09 21:00:17 +0000

    stop

--- stop.py
+++ stop.py
@@ -12,7 +12,7 @@ from dsautils import dsa_store
 d = dsa_store.DsaStore()
 d.put_dict('/cmd/corr/0', {'cmd': 'trigger', 'val': '0-flush-'})
 pytime.sleep(120)
-os.system('/usr/local/bin/dsacon corr stop')
+os.system('/home/ubuntu/anaconda3/envs/casa38/bin/dsacon corr stop')
 pytime.sleep(60)
 d.put_dict('/cmd/corr/docopy','False')
 pytime.sleep(10)
